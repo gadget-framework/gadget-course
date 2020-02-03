312402484f3c6c79925454e732c520f38d418257
Jamie Lentin <jm@lentin.co.uk>
2020-02-03 15:03:14 +0000

    install-deps: More dependencies

--- install-deps.R
+++ install-deps.R
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
 
+if (!require('gadget2')) install.packages('Hafro/gadget')
+if (!require('Rgadget')) install.packages('Hafro/Rgadget')
 if (!require('nycflights13')) install.packages('nycflights13')
 if (!require('infuser')) install.packages('https://cran.r-project.org/src/contrib/Archive/infuser/infuser_0.2.8.tar.gz', repos=NULL, type="source")
-
+if (!require('patchwork')) install.packages('patchwork')

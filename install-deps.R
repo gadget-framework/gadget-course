f65287434ee7cda83b6c3d6dae907d9a496a8afd
bthe <bjarki.elvarsson@gmail.com>
2020-02-04 15:50:49 +0000

    added example of MFDB upload to the MFDB tutorial

--- install-deps.R
+++ install-deps.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-if (!require('gadget2')) install.packages('Hafro/gadget')
+if (!require('gadget2')) install.packages('Hafro/gadget2')
 if (!require('Rgadget')) install.packages('Hafro/Rgadget')
 if (!require('nycflights13')) install.packages('nycflights13')
 if (!require('infuser')) install.packages('https://cran.r-project.org/src/contrib/Archive/infuser/infuser_0.2.8.tar.gz', repos=NULL, type="source")
